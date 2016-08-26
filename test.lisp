a22395da39a9419eb51c79730d1fc41369be2718
leosongwei <leo_songwei@outlook.com>
2016-08-26 09:37:14 +0800

    test.lisp

--- test.lisp
+++ test.lisp
@@ -1,19 +1,25 @@
 (slide
   (body
-    (bold "Slider")
-    (br)
+    (reverse (bold "Slider: 用S表达式制作朴实的幻灯片")) (br)
     (brer
       "* slider是一个播放幻灯片用的程序"
-      "* slider有自己的DSL"
+      "* slider的DSL基于S表达式"
       "* slider通过Lisp的宏功能生成代码")))
 
 (slide
   :background yellow
-  (body "这一页只有一行"))
+  (body (reverse (bold "操作说明")) (br)
+        (brer
+          (color green "* j: 下一页")
+          (color green "* k: 上一页")
+          (color red "* R: 重新载入（我还没有调试）")
+          (color blue "* q: 退出Slider"))))
 
 (slide :background red
   (body (reverse (bold "Slider是一个花了8小时写成的程序！！")) (br)
         (brer
           "* 不稳定，使用后果自负"
+          "* 睡眠不足"
           "* 意识流编程"
+          "* 很多bug"
           "* 简陋的代码")))
